aa121d97738d9519d68c1a3ea5c1f8c9b13a432c
Eric Zhang <qiang.zhang.sh2@dianping.com>
2016-05-10 16:58:16 +0800

    no message

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by madlord on 16/1/27.
+ */
+(function () {
+    function upadte() {
+        let w=Math.min(window.screen.width,document.body.clientWidth,document.documentElement.getBoundingClientRect().width);
+        document.getElementsByTagName('html')[0].style['font-size']=w/375*0.625*16+'px'
+    }
+    module.exports={
+        init:function () {
+            window.addEventListener('resize',ResponseUtil.upadte)
+            upadte();
+        },
+
+        getOneREMSize:function () {
+            return parseFloat(document.getElementsByTagName('html')[0].style['font-size']);
+        }
+    }
+})();
+
