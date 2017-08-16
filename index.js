b0f38074b7b6f72cf0d0e60062c45a3c307c9aa4
453949887@qq.com <453848776@qq.com>
2017-08-16 13:08:09 +0800

    deal ios8 getcomputedstyle not right fontsize val question

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 /**
  * Created by madlord on 16/1/27.
  */
-var baseWidth=375;
-var baseRemSize=10;
+
 (function () {
+    if (typeof window === "undefined") return;
+    var baseWidth=375;
+    var baseRemSize=10;
     function upadte() {
         var w=Math.min(window.screen.width,document.body.clientWidth,document.documentElement.getBoundingClientRect().width);
-        document.getElementsByTagName('html')[0].style['font-size']=w/baseWidth*baseRemSize+'px'
+        var fontSize=w/baseWidth*baseRemSize;
+        document.documentElement.style['fontSize']=fontSize+'px';
+        var div=document.createElement("div");
+        div.style.width=fontSize+'px';
+        div.id='rem';
+        document.body.appendChild(div);
+        var realFontSize=parseFloat(document.defaultView.getComputedStyle(document.getElementById('rem'), null)['width']);
+
+        //部分华为魅族机型的html fontSize计算不正确
+        if (realFontSize/fontSize!==1) {
+
+            document.documentElement.style['fontSize']=fontSize*fontSize/realFontSize+'px';
+        }
+
     }
     module.exports={
         init:function (bw) {
@@ -20,5 +35,4 @@ var baseRemSize=10;
         }
     }
 })();
-console.log("======")
 
